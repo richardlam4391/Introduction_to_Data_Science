db8dc549c0b9401e73692c6047d4b5f932889d99
Richard Lam <81891771+richardlam4391@users.noreply.github.com>
2026-07-08 15:00:40 -0400

    Add query to identify most crime-prone area
    Query to find the most crime-prone community area in Chicago.

--- Databases and SQL for Data Science with Python/Chicago Census Data 8.sql
+++ Databases and SQL for Data Science with Python/Chicago Census Data 8.sql
@@ -1,3 +1,4 @@
+-- Which community area is most crime prone? Display the coumminty area number only.
 SELECT community_area_number FROM sample_csv.chicagocrimedata 
 where community_area_number is not null
 group by community_area_number 
